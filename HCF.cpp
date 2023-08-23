819ed26bdfeb971c88b228f3ccd47b2282ffc38a
Kahish <kashishkaramchandani3@gmail.com>
2023-08-24 02:42:27 +0530

    code to find gcd

---
+++ HCF.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1;
+	int i;
+	int n2;
+	int gcd;
+	cin>>n1>>n2;
+	for(i=1;i<=n1;i++)
+	{
+	
+	if (n1%i==0 && n2%i==0)
+	{
+		gcd=i;
+    }
+}
+cout<<gcd;
+}
+	
+
